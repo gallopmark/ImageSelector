81bed11b3143df07a68e0a210ce6ec03e03ab65a
tthwy <i23xiaoyan@qq.com>
2020-03-18 17:07:38 +0800

    v1.0.0

---
+++ library/src/main/java/com/gallopmark/imagepicker/adapter/BaseViewHolder.java
@@ -0,0 +1,12 @@
+package com.gallopmark.imagepicker.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+
+class BaseViewHolder extends RecyclerView.ViewHolder {
+
+    BaseViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
